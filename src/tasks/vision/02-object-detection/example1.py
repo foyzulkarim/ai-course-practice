26fafd82a71f6db470a810246d3193e16e106302
Foyzul Karim <foyzulkarim@gmail.com>
2025-02-22 10:56:38 +1100

    feat(object-detection): Introduce model selection using Enum for object detection pipeline

--- src/tasks/vision/02-object-detection/example1.py
+++ src/tasks/vision/02-object-detection/example1.py
@@ -1,9 +1,19 @@
+from enum import Enum
 from PIL import Image
 from transformers import pipeline
 
+# Enum of the model names
+class ModelName(Enum):
+    DETR_RESNET_50 = "facebook/detr-resnet-50"
+    DETR_TABLE_TRANSFORMER = "microsoft/table-transformer-detection"
+
 def main():
-    # Initialize the object detection pipeline
-    object_detector = pipeline("object-detection")
+    # Initialize the object detection pipeline, default: facebook/detr-resnet-50
+    object_detector = pipeline("object-detection", model=ModelName.DETR_RESNET_50.value)
+
+     # print model name
+    print('Model name:')
+    print(object_detector.model.config.name_or_path)
     
     # Load an example image from a local file
     image_path = "../images/food.jpg"
