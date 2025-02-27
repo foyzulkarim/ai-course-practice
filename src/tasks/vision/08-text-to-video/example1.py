5e35a385a99b50fdc0f75052d1f2ec7e02128332
Foyzul Karim <foyzulkarim@gmail.com>
2025-02-27 16:24:18 +1100

    feat: update text-to-video example and requirements, add video export functionality

--- src/tasks/vision/08-text-to-video/example1.py
+++ src/tasks/vision/08-text-to-video/example1.py
@@ -1,21 +1,23 @@
 import torch
 from diffusers import AnimateDiffPipeline, MotionAdapter, EulerDiscreteScheduler
-from diffusers.utils import export_to_gif
+from diffusers.utils import export_to_gif, export_to_video
 from huggingface_hub import hf_hub_download
 from safetensors.torch import load_file
 
 device = "mps" # Options: ["cpu", "cuda", "mps"]
 dtype = torch.float16
 
-step = 2  # Options: [1,2,4,8]
+step = 1  # Options: [1,2,4,8]
 repo = "ByteDance/AnimateDiff-Lightning"
 ckpt = f"animatediff_lightning_{step}step_diffusers.safetensors"
 
-# base = "emilianJR/epiCRealism"  # Choose to your favorite base model.
+# epiCRealism - worked [step=1,2,4]
+base = "emilianJR/epiCRealism" 
+fileName = "e1_animatediff_4step.gif"
 
 # ToonYou - worked [step=1,2]
-base = "frankjoshua/toonyou_beta6"
-fileName = "e1_toonyou_2step.gif"
+# base = "frankjoshua/toonyou_beta6"
+# fileName = "e1_toonyou_2step.gif"
 
 # SG161222/Realistic_Vision_V5.1_noVAE
 # base = "SG161222/Realistic_Vision_V5.1_noVAE"
@@ -25,8 +27,6 @@ fileName = "e1_toonyou_2step.gif"
 # base = "Yntec/ResidentCNZCartoon3D"
 # fileName = "e1_animatediff_1step.gif"
 
-# guoyww/animatediff-motion-adapter-v1-5-2
-# base = "guoyww/animatediff-motion-adapter-v1-5-2"
 
 adapter = MotionAdapter().to(device, dtype)
 adapter.load_state_dict(load_file(hf_hub_download(repo ,ckpt), device=device))
@@ -34,8 +34,9 @@ pipe = AnimateDiffPipeline.from_pretrained(base, motion_adapter=adapter, torch_d
 pipe.scheduler = EulerDiscreteScheduler.from_config(pipe.scheduler.config, timestep_spacing="trailing", beta_schedule="linear")
 
 prompts = """
-Generate an anime-style video of a dragon flying over a vibrant jungle. Style references: Hayao Miyazaki. Output resolution: 1080p, 60fps.
+A panda, dressed in a small, red jacket and a tiny hat, sits on a wooden stool in a serene bamboo forest. The panda's fluffy paws strum a miniature acoustic guitar, producing soft, melodic tunes.Sunlight filters through the tall bamboo, casting a gentle glow on the scene.
 """
 
 output = pipe(prompt=prompts, guidance_scale=0.8, num_inference_steps=step)
 export_to_gif(output.frames[0], fileName)
+# export_to_video(output.frames[0], "output.mp4")
